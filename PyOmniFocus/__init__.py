0ae2d4716744a5c73b643805fdb66a4ec9543bde
Keith Hutchison <keith.hutchison@balance-infosystems.com>
2012-12-17 19:01:20 +1100

    Removed comments not related to PyOmniFocus

--- PyOmniFocus/__init__.py
+++ PyOmniFocus/__init__.py
@@ -1,11 +1,3 @@
-# PyFileMaker - Integrating FileMaker and Python 
-# (c) 2006-2008 Klokan Petr Pridal, klokan@klokan.cz 
-# (c) 2002-2006 Pieter Claerhout, pieter@yellowduck.be
-# 
-# http://code.google.com/p/pyfilemaker/
-# http://www.yellowduck.be/filemaker/
+__version__ = "0.0.1-dev"
 
-__version__ = "2.6"
-
-# Import the FileMaker core modules
 import omnifocus
